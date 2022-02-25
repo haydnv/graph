c429d8504961cd11e97806d50715197b9fec2a60
haydnv <haydnv@gmail.com>
2022-02-25 23:00:57 +0000

    rewrite Graph to eliminate mutable Placeholder type

--- examples/example.rs
+++ examples/example.rs
@@ -1,21 +1,86 @@
+use std::convert::{TryFrom, TryInto};
+use std::fmt;
+use std::fmt::{Display, Formatter};
+
 use async_graph::*;
+use hr_id::Id;
+
+#[derive(Debug)]
+struct Error(String);
+
+impl std::error::Error for Error {}
+
+impl async_graph::Error for Error {
+    fn consume(self, id: &Id) -> Self {
+        todo!()
+    }
+
+    fn collision<E: Display, N: Display>(id: &Id, existing: E, new: N) -> Self {
+        Self(format!(
+            "there is already a value {} for {} (not {})",
+            existing, id, new
+        ))
+    }
+
+    fn not_found(id: &Id) -> Self {
+        Self(format!("not found: {}", id))
+    }
+
+    fn invalid_type<E: fmt::Display, A: fmt::Display>(expected: E, actual: A) -> Self {
+        Self(format!("invalid type {}, expected {}", actual, expected))
+    }
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        fmt::Display::fmt(&self.0, f)
+    }
+}
+
+#[derive(Clone)]
+enum State {
+    Number(i32),
+    String(String),
+}
+
+impl async_graph::State for State {
+    type Target = Self;
+}
+
+impl From<String> for State {
+    fn from(s: String) -> Self {
+        Self::String(s)
+    }
+}
+
+impl TryFrom<State> for i32 {
+    type Error = Error;
+
+    fn try_from(value: State) -> Result<Self, Self::Error> {
+        match value {
+            State::Number(i) => Ok(i),
+            other => Err(async_graph::Error::invalid_type("i32", other)),
+        }
+    }
+}
+
+impl fmt::Display for State {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Self::Number(i) => fmt::Display::fmt(i, f),
+            Self::String(s) => fmt::Display::fmt(s, f),
+        }
+    }
+}
 
 fn concat(inputs: (String, String)) -> String {
     format!("{} {}", inputs.0, inputs.1)
 }
 
 fn main() {
-    let hello = "hello".to_owned();
-    let mut name = Placeholder::<String>::new();
+    let mut graph = Graph::<State, Error>::new();
 
-    let inputs = (hello, name.clone());
-    let hello_name: Op<'_, (String, String), String> = Op::new(inputs, concat);
-
-    let world = "world".to_string();
-    name.provide(world);
-    assert_eq!(hello_name.eval(), "hello world");
-
-    let test = "test".to_string();
-    name.provide(test);
-    assert_eq!(hello_name.eval(), "hello test");
+    graph
+        .add_state("hello".parse().expect("id"), "hello".to_string().into())
+        .unwrap();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,65 +1,61 @@
-use std::sync::{Arc, RwLock};
+use std::collections::hash_map::Entry;
+use std::collections::HashMap;
+use std::convert::Infallible;
+use std::fmt;
+
+use hr_id::Id;
+
+pub trait Error: std::error::Error {
+    fn consume(self, id: &Id) -> Self;
+
+    fn collision<E: fmt::Display, N: fmt::Display>(id: &Id, existing: E, new: N) -> Self;
+
+    fn not_found(id: &Id) -> Self;
+
+    fn invalid_type<E: fmt::Display, A: fmt::Display>(expected: E, actual: A) -> Self;
+}
 
 pub trait Node {
     type State;
+    type Error;
 
-    fn eval(&self) -> Self::State;
+    fn eval(&self) -> Result<Self::State, Self::Error>;
 }
 
 pub trait State: Sized {
     type Target;
 }
 
-#[macro_export]
-macro_rules! state {
-    ($t:ty) => {
-        impl State for $t {
-            type Target = $t;
-        }
-
-        impl Node for $t {
-            type State = $t;
-
-            fn eval(&self) -> $t {
-                self.clone()
-            }
-        }
-    };
-}
-
-state!(());
-state!(String);
-
-impl<T1, T2> State for (T1, T2)
+impl<T> Node for T
 where
-    T1: State,
-    T2: State,
+    T: State<Target = T> + Clone,
 {
-    type Target = (T1::Target, T2::Target);
-}
+    type State = T;
+    type Error = Infallible;
 
-impl<T1, T2> Node for (T1, T2)
-where
-    T1: Node,
-    T2: Node,
-{
-    type State = (T1::State, T2::State);
-
-    fn eval(&self) -> Self::State {
-        (self.0.eval(), self.1.eval())
+    fn eval(&self) -> Result<T, Infallible> {
+        Ok(self.clone())
     }
 }
 
-pub struct Op<'a, I, O> {
-    input: Box<dyn Node<State = I> + 'a>,
-    def: Box<dyn Fn(I) -> O + 'a>,
+impl State for () {
+    type Target = ();
 }
 
-impl<'a, I, O> Op<'a, I, O> {
+impl State for String {
+    type Target = String;
+}
+
+pub struct Op<'a, I, O, E> {
+    input: Box<dyn Node<State = I, Error = E> + 'a>,
+    def: Box<dyn Fn(I) -> Result<O, E> + 'a>,
+}
+
+impl<'a, I, O, E> Op<'a, I, O, E> {
     pub fn new<N, F>(input: N, def: F) -> Self
     where
-        N: Node<State = I> + 'a,
-        F: Fn(I) -> O + 'a,
+        N: Node<State = I, Error = E> + 'a,
+        F: Fn(I) -> Result<O, E> + 'a,
     {
         Self {
             input: Box::new(input),
@@ -68,45 +64,57 @@ impl<'a, I, O> Op<'a, I, O> {
     }
 }
 
-impl<'a, I, O> Node for Op<'a, I, O> {
+impl<'a, I, O, E> Node for Op<'a, I, O, E> {
     type State = O;
+    type Error = E;
 
-    fn eval(&self) -> O {
-        let input = self.input.eval();
+    fn eval(&self) -> Result<O, E> {
+        let input = self.input.eval()?;
         (self.def)(input)
     }
 }
 
-#[derive(Clone)]
-pub struct Placeholder<T> {
-    state: Arc<RwLock<Option<T>>>,
+trait Placeholder {}
+
+impl Placeholder for Id {}
+
+impl Placeholder for (Id, Id) {}
+
+trait Resolve<'a, T, E> {
+    fn resolve<P: Placeholder>(
+        &self,
+        placeholder: P,
+    ) -> Option<Box<dyn Node<State = T, Error = E>>>;
 }
 
-impl<'a, T: Clone> Placeholder<T> {
+pub struct Graph<'a, T, E> {
+    states: HashMap<Id, T>,
+    ops: HashMap<Id, (Box<dyn Placeholder>, Box<dyn Fn(T) -> Result<T, E> + 'a>)>,
+}
+
+impl<'a, T, E> Graph<'a, T, E>
+where
+    T: State + Clone + fmt::Display,
+    E: Error,
+{
     pub fn new() -> Self {
         Self {
-            state: Arc::new(RwLock::new(None)),
+            states: HashMap::new(),
+            ops: HashMap::new(),
         }
     }
 
-    pub fn provide(&mut self, value: T) {
-        let mut state = self.state.write().expect("placeholder");
-        *state = Some(value);
+    pub fn add_state(&mut self, id: Id, state: T) -> Result<(), E> {
+        match self.states.entry(id) {
+            Entry::Vacant(vacant) => {
+                vacant.insert(state);
+                Ok(())
+            }
+            Entry::Occupied(existing) => Err(E::collision(existing.key(), existing.get(), state)),
+        }
     }
-}
 
-impl<'a, T> State for Placeholder<T> {
-    type Target = T;
-}
-
-impl<'a, T> Node for Placeholder<T>
-where
-    T: Clone,
-{
-    type State = T;
-
-    fn eval(&self) -> Self::State {
-        let state = self.state.read().expect("placeholder lock");
-        state.as_ref().expect("input").clone()
+    pub fn build(self, inputs: HashMap<Id, T>) -> Box<dyn Node<State = T, Error = E>> {
+        todo!()
     }
 }
